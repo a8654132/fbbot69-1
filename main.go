99205002e4a9e37d39cd4dabc82ab616af984522
a8654132 <31846955+a8654132@users.noreply.github.com>
2017-10-11 05:41:15 +0800

    z

--- main.go
+++ main.go
@@ -18,10 +18,11 @@ func main() {
 	mess.VerifyToken = os.Getenv("TOKEN")
 	mess.AccessToken = os.Getenv("TOKEN")
 	log.Println("Bot start in token:", mess.VerifyToken)
+	mess.MessageReceived = MessageReceived
+	http.HandleFunc("/webhook", mess.Handler)
 	mess.SendSimpleMessage("1460870680701162", fmt.Sprintf("如果你看到這個，\n就代表我成功主動傳送訊息囉！"))
 	// mess.MessageReceived = handler
 
-	http.HandleFunc("/webhook", mess.Handler)
 
 	// button := NewWebURLButton("點此看阿卡莉", "https://www.pixiv.net/member_illust.php?mode=medium&illust_id=62861397")
 	// mess.SendSimpleMessage("1460870680701162", button)
@@ -50,13 +51,13 @@ func main() {
 
 
 
-//
-//   // MessageReceived :Callback to handle when message received.
-// func MessageReceived(event Event, opts MessageOpts, msg ReceivedMessage) {
-// 	// log.Println("event:", event, " opt:", opts, " msg:", msg)
-// 	resp, err := mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("你好，現在是被動的回復訊息。\n你的ID為%s\n你剛剛說的話為：%s", opts.Sender.ID ,msg.Text))
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	fmt.Printf("%+v", resp)
-// }
+
+  // MessageReceived :Callback to handle when message received.
+func MessageReceived(event Event, opts MessageOpts, msg ReceivedMessage) {
+	// log.Println("event:", event, " opt:", opts, " msg:", msg)
+	resp, err := mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("你好，現在是被動的回復訊息。\n你的ID為%s\n你剛剛說的話為：%s", opts.Sender.ID ,msg.Text))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%+v", resp)
+}
