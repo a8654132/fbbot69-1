6b18bc38e80499538776f0abb8fef27de17b209f
a8654132 <31846955+a8654132@users.noreply.github.com>
2017-10-11 04:19:43 +0800

    456

---
+++ receipt_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestReceiptType(t *testing.T) {
+	template := &ReceiptTemplate{}
+	if template.Type() != TemplateTypeReceipt {
+		t.Error("Receipt template returned invalid type")
+	}
+	if template.SupportsButtons() {
+		t.Error("Button template is marked as supporting buttons.")
+	}
+}
