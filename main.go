2880cb1cf489a3d4ad3fc990cf1ef60128f1d62a
a8654132 <31846955+a8654132@users.noreply.github.com>
2017-10-11 03:21:01 +0800

    ?

--- main.go
+++ main.go
@@ -17,13 +17,14 @@ func main() {
 	log.Println("Bot start in token:", mess.VerifyToken)
 	mess.MessageReceived = MessageReceived
 	http.HandleFunc("/webhook", mess.Handler)
+	mess.SendSimpleMessage("1460870680701162", fmt.Sprintf("主動傳送訊息!"))
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 //MessageReceived :Callback to handle when message received.
 func MessageReceived(event Event, opts MessageOpts, msg ReceivedMessage) {
 	// log.Println("event:", event, " opt:", opts, " msg:", msg)
-	resp, err := mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("你好,現在是被動的,%s,%s", opts.Sender.ID ,msg.Text))
+	resp, err := mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("你好!現在是被動的:%s.%s", opts.Sender.ID ,msg.Text))
 	if err != nil {
 		fmt.Println(err)
 	}
