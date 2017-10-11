1d309cb4bc78d287a000896e93e9481930ecf057
a8654132 <31846955+a8654132@users.noreply.github.com>
2017-10-11 12:43:11 +0800

    123

--- main.go
+++ main.go
@@ -42,7 +42,8 @@ func MessageReceived(event messenger.Event, opts messenger.MessageOpts, msg mess
 					Title:   "點此看阿卡莉",
 					URL:		 "https://www.pixiv.net/member_illust.php?mode=medium&illust_id=62861397",
 				},
-			})
+			},
+		})
 		resp, err := mess.SendMessage(mq)
 		if err != nil {
 			fmt.Println(err)
