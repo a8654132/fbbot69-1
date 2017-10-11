9cb15e87ffa3d81ce272fbb412115222c6b32712
a8654132 <31846955+a8654132@users.noreply.github.com>
2017-10-11 12:19:30 +0800

    87秉翰

--- main.go
+++ main.go
@@ -35,8 +35,8 @@ func MessageReceived(event messenger.Event, opts messenger.MessageOpts, msg mess
 		mq.RecipientID("1460870680701162")
 		mq.Template(template.GenericTemplate {Title: "abc",
 			Buttons: NewWebURLButton("點此看阿卡莉", "https://www.pixiv.net/member_illust.php?mode=medium&illust_id=62861397")
-			},
-		})
+			}
+		)
 		resp, err := mess.SendMessage(mq)
 		if err != nil {
 			fmt.Println(err)
