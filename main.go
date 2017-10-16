8dc635ebb68d9eafdf7a3ff9c3e6462e7bb68da9
a8654132 <31846955+a8654132@users.noreply.github.com>
2017-10-16 23:09:24 +0800

    取得ID

--- main.go
+++ main.go
@@ -5,10 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"encoding/json"
-
 	"github.com/maciekmm/messenger-platform-go-sdk"
-	// "github.com/maciekmm/messenger-platform-go-sdk/template"
 )
 
 var mess = &messenger.Messenger{}
@@ -21,63 +18,15 @@ func main() {
 	log.Println("Bot start in token:", mess.VerifyToken)
 
 	http.HandleFunc("/webhook", mess.Handler)
-	SendButton();
-	// mess.MessageReceived = handler
+	mess.MessageReceived = handler
 
-
-	// button := NewWebURLButton("點此看阿卡莉", "https://www.pixiv.net/member_illust.php?mode=medium&illust_id=62861397")
-	// mess.SendSimpleMessage("1460870680701162", button)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-
-func SendButton() {
-
-		binary, _ := Redis_Get(mac)
-		user := new(USER_MAC)
-		json.Unmarshal(binary,&user)
-
-		for i:=0 ; i< len(user.USER) ; i++ {
-
-		// for i:=0 ; i< 2 ; i++ {
-			// mq := messenger.MessageQuery{}
-			onlyonecontent := user.USER[i].CONTENT
-			// mq.RecipientID("1460870680701162")
-			// mq.Template(template.GenericTemplate {Title: "請告訴我們您是否滿意這篇文章：",
-			// 	Buttons: []template.Button{
-			// 		template.Button{
-			// 			Type:    template.ButtonTypePostback,
-			// 			Payload: "good",
-			// 			Title:   "滿意",
-			// 		},
-			// 		template.Button{
-			// 			Type:    template.ButtonTypePostback,
-			// 			Payload: "bad",
-			// 			Title:   "不滿意",
-			// 		},
-			// 		template.Button{
-			// 			Type:    template.ButtonTypeWebURL,
-			// 			Title:   "點此開啟網頁",
-			// 			URL:		 user.USER[i].NAME,
-			// 		},
-			// 	},
-			// })
-			x := fmt.Sprintf("%d", i+1)
-			mess.SendSimpleMessage("1460870680701162", x + ".\n" + onlyonecontent )
-			// mess.SendMessage(mq)
-		}
-
+func MessageReceived(event messenger.Event, opts messenger.MessageOpts, msg messenger.ReceivedMessage) {
+	resp, err := mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("你好，現在是被動的回復訊息。\n你的ID為%s\n你剛剛說的話為：%s", opts.Sender.ID ,msg.Text))
+	if err != nil {
+		fmt.Println(err)
 	}
-
-
-
-//
-//   // MessageReceived :Callback to handle when message received.
-// func MessageReceived(event messenger.Event, opts messenger.MessageOpts, msg messenger.ReceivedMessage) {
-// 	// log.Println("event:", event, " opt:", opts, " msg:", msg)
-// 	resp, err := mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("你好，現在是被動的回復訊息。\n你的ID為%s\n你剛剛說的話為：%s", opts.Sender.ID ,msg.Text))
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	fmt.Printf("%+v", resp)
-// }
+	fmt.Printf("%+v", resp)
+}
