47a573f655f88c0a9e971c8fce365442bb6c9b8f
a8654132 <31846955+a8654132@users.noreply.github.com>
2017-10-24 00:48:01 +0800

    45

--- main.go
+++ main.go
@@ -20,9 +20,6 @@ func main() {
 	mess.AccessToken = os.Getenv("TOKEN")
 	log.Println("Bot start in token:", mess.VerifyToken)
 
-	mess.SendSimpleMessage("1460870680701162", "這是主動的，傳給1460870680701162")
-	mess.SendSimpleMessage("2122993047726622", "這是主動的，傳給2122993047726622")
-
 	http.HandleFunc("/webhook", mess.Handler)
 	mess.MessageReceived = MessageReceived
 
@@ -31,9 +28,9 @@ func main() {
 
 
 func MessageReceived(event messenger.Event, opts messenger.MessageOpts, msg messenger.ReceivedMessage) {
-	stringid := fmt.Sprintf("%s",opts.Sender.ID)
-	content := Redis_IDtoMAC(stringid)
-	resp, err := mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("你好，現在是被動的回復訊息。\n你的ID為%s\n你剛剛說的話為：%s\n\n%s", opts.Sender.ID , msg.Text,content))
+	// stringid := fmt.Sprintf("%s",opts.Sender.ID)
+	// content := Redis_IDtoMAC(stringid)
+	resp, err := mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("你好，現在是被動的回復訊息。\n你的ID為%s\n你剛剛說的話為：%s\n\n", opts.Sender.ID , msg.Text))
 	if err != nil {
 		fmt.Println(err)
 	}
