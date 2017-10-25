3ebdbb7ef23c246124fb286e8db50021c303026e
a8654132 <31846955+a8654132@users.noreply.github.com>
2017-10-25 13:35:38 +0800

    222

--- main.go
+++ main.go
@@ -30,11 +30,11 @@ func main() {
 func MessageReceived(event messenger.Event, opts messenger.MessageOpts, msg messenger.ReceivedMessage) {
 	stringid := fmt.Sprintf("%s",opts.Sender.ID)
 	content := Redis_IDtoMAC(stringid)
+	fmt.Println(content)
 
 	mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("你的id為:%s",opts.Sender.ID))
 
-		for a:=0 ; a< len(content) ; a++{
-			b := fmt.Sprintf("%d",a)
-			mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("%s,%s",b,content[a]))
-		}
+	for i:=0 ; i< len(content) ; i++{
+			mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("%s",content[i]))
+	}
 }
